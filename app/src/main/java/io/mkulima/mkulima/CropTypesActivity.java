d08881e774a3652d4d9c0a7fe036235ad95c2120
Walter Sanchez <walteranyika@gmail.com>
2018-09-01 10:25:54 +0300

    Added Crop Types

--- app/src/main/java/io/mkulima/mkulima/CropTypesActivity.java
+++ app/src/main/java/io/mkulima/mkulima/CropTypesActivity.java
@@ -2,6 +2,7 @@ package io.mkulima.mkulima;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class CropTypesActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class CropTypesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crop_types);
+        WebView webView=findViewById(R.id.webView);
+        webView.loadUrl("file:///android_asset/onion_types.html");
     }
 }
